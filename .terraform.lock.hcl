34a309a36b4955ba3b56200fce5d3255f21f2a32
tero2355 <51174562+tero2355@users.noreply.github.com>
2023-12-11 10:20:01 +0200

    initial connectivity (#1)
    * initial connectivity
    
    * format changes

---
+++ .terraform.lock.hcl
@@ -0,0 +1,82 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/azure/azapi" {
+  version     = "1.10.0"
+  constraints = ">= 1.3.0"
+  hashes = [
+    "h1:i6CNMRDuQ1RdYxamyfaRwjz0nb3LoSqihzDe0RlBAcU=",
+    "zh:0852c7e4701a420be84380351bcc3b8b41a1f550916deb6b927fbd97a376a8aa",
+    "zh:096ca824ab7ce61a3825335352a41e443b3355979dafb37eeded8d829ae6a6ba",
+    "zh:2c243d610e522acbecf6193653226c322e6ee35f3a7f95d11373533de9eeae2e",
+    "zh:38dc218eba4f426aff1a3ed9ff8e93bf753fea84423b198fa66a6438d8c9b36c",
+    "zh:5072dd8d446aa00aafc3b007b4947e2cde87c1fe6449ec21cfe360b07e8d3dc2",
+    "zh:6b345207665080642dedc4387e9affefbd9093d4812cf7a57f8009a7ab8f2383",
+    "zh:6f42952c5e77eb3ed59480817b220fe16768cb593740386c20e3dec90c5feee2",
+    "zh:7bfb6d9940d7ddd1e7b9851fb54c5157c118b7d83b870f7db3e0743a284c5453",
+    "zh:a92b399c488d767ad0375141a0c4c75a1a829d18442fd78097d1a91bf55264d0",
+    "zh:bc8ba158f7495cb2ac1d25d8b7217780d9d2d070afc69b4148b457a1ea5c3f14",
+    "zh:f542964189f21e2fc4250756dcfd798a336bd1bac8eb7e707fe9836ac7b790b6",
+    "zh:f7e08ee1e69f500bdb7d0b349d4489c989b415044765ae10a8ba329b76e64731",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.84.0"
+  constraints = ">= 3.0.0, >= 3.74.0"
+  hashes = [
+    "h1:3KYwbI62e6u2f7ob9Ps8yahnIaNHkE56UsF0130zRzE=",
+    "zh:14a96daf672541dbc27137d9cc0a96a737710597262ecaaa64a328eb1174e5df",
+    "zh:16d8e794fdd87ed8e64291fe8a617f420d8263f21672033333a020d06f4c9618",
+    "zh:64e5cd1bb6a81bccffff0d1f77790286ab46179cf12442134c3f3bca722afc1b",
+    "zh:7010ada67fbae971ac8b7204a30b1317aee7ccac7227afc6ac27277c642996a1",
+    "zh:77c2616ecd29685d2a4dc3ec3e9771e5ecf652e127946767d9b7ef19bbf58a21",
+    "zh:861922cfae724eacf1bd915efd5dbf6c23e4e762a2bbe60993099648e64aedb5",
+    "zh:8fb797c98bb08e7342995317810d28c41bb519fbc128adaa170896356b9eaebd",
+    "zh:982e85a4a9d282e3c8f7d7836037ccc98ff3ef50af246fad2e04684a81d16201",
+    "zh:a2ef29ff907cf6622e58afa0a27e23a3160ba3d70d531795b4d9a6c42c354630",
+    "zh:c46ccc4eecb79d096bcb652af0cffe300ec480d80a13a5b302c71b1aac9f9f1c",
+    "zh:cc6a06bf6d5e811fe8c0d9ad652e143b4e94bd16a03fb8a86f5086f0ae5abfa9",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.6.0"
+  constraints = ">= 3.1.0"
+  hashes = [
+    "h1:t0mRdJzegohRKhfdoQEJnv3JRISSezJRblN0HIe67vo=",
+    "zh:03360ed3ecd31e8c5dac9c95fe0858be50f3e9a0d0c654b5e504109c2159287d",
+    "zh:1c67ac51254ba2a2bb53a25e8ae7e4d076103483f55f39b426ec55e47d1fe211",
+    "zh:24a17bba7f6d679538ff51b3a2f378cedadede97af8a1db7dad4fd8d6d50f829",
+    "zh:30ffb297ffd1633175d6545d37c2217e2cef9545a6e03946e514c59c0859b77d",
+    "zh:454ce4b3dbc73e6775f2f6605d45cee6e16c3872a2e66a2c97993d6e5cbd7055",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:91df0a9fab329aff2ff4cf26797592eb7a3a90b4a0c04d64ce186654e0cc6e17",
+    "zh:aa57384b85622a9f7bfb5d4512ca88e61f22a9cea9f30febaa4c98c68ff0dc21",
+    "zh:c4a3e329ba786ffb6f2b694e1fd41d413a7010f3a53c20b432325a94fa71e839",
+    "zh:e2699bc9116447f96c53d55f2a00570f982e6f9935038c3810603572693712d0",
+    "zh:e747c0fd5d7684e5bfad8aa0ca441903f15ae7a98a737ff6aca24ba223207e2c",
+    "zh:f1ca75f417ce490368f047b63ec09fd003711ae48487fba90b4aba2ccf71920e",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version     = "0.10.0"
+  constraints = ">= 0.7.0"
+  hashes = [
+    "h1:rPbqd7mEyBetQPjyBDBAdwiARulKbh2LwzQp2GSl/AI=",
+    "zh:0ab31efe760cc86c9eef9e8eb070ae9e15c52c617243bbd9041632d44ea70781",
+    "zh:0ee4e906e28f23c598632eeac297ab098d6d6a90629d15516814ab90ad42aec8",
+    "zh:3bbb3e9da728b82428c6f18533b5b7c014e8ff1b8d9b2587107c966b985e5bcc",
+    "zh:6771c72db4e4486f2c2603c81dfddd9e28b6554d1ded2996b4cb37f887b467de",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:833c636d86c2c8f23296a7da5d492bdfd7260e22899fc8af8cc3937eb41a7391",
+    "zh:c545f1497ae0978ffc979645e594b57ff06c30b4144486f4f362d686366e2e42",
+    "zh:def83c6a85db611b8f1d996d32869f59397c23b8b78e39a978c8a2296b0588b2",
+    "zh:df9579b72cc8e5fac6efee20c7d0a8b72d3d859b50828b1c473d620ab939e2c7",
+    "zh:e281a8ecbb33c185e2d0976dc526c93b7359e3ffdc8130df7422863f4952c00e",
+    "zh:ecb1af3ae67ac7933b5630606672c94ec1f54b119bf77d3091f16d55ab634461",
+    "zh:f8109f13e07a741e1e8a52134f84583f97a819e33600be44623a21f6424d6593",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,24 +1,170 @@
+
+
+
+
+
 module "enterprise-scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "4.2.0"
+  version = "5.0.3"
 
   disable_telemetry = true
 
   default_location = var.default_location
   root_parent_id   = data.azurerm_client_config.core.tenant_id
 
-  deploy_corp_landing_zones    = true
-  deploy_management_resources  = true
-  deploy_online_landing_zones  = true
-  root_id                      = var.root_id
-  root_name                    = var.root_name
-  subscription_id_connectivity = var.subscription_id_connectivity
-  subscription_id_identity     = var.subscription_id_identity
-  subscription_id_management   = var.subscription_id_management
+  deploy_corp_landing_zones     = true
+  deploy_management_resources   = true
+  deploy_online_landing_zones   = true
+  deploy_connectivity_resources = true
+  root_id                       = var.root_id
+  root_name                     = var.root_name
+  subscription_id_connectivity  = var.subscription_id_connectivity
+  subscription_id_identity      = var.subscription_id_identity
+  subscription_id_management    = var.subscription_id_management
 
   providers = {
     azurerm              = azurerm
     azurerm.connectivity = azurerm.connectivity
     azurerm.management   = azurerm.management
   }
+
+  # Configure custom settings for the module to deploy Virtual WAN hub
+  # network resources instead of traditional hub network resources.
+  configure_connectivity_resources = {
+    settings = {
+      hub_networks = []
+      vwan_hub_networks = [
+        {
+          enabled = true
+          config = {
+            address_prefix = "10.200.0.0/22"
+            location       = ""
+            sku            = "Basic"
+            routes         = []
+            expressroute_gateway = {
+              enabled = false
+              config = {
+                scale_unit = 1
+              }
+            }
+            vpn_gateway = {
+              enabled = true
+              config = {
+                bgp_settings       = []
+                routing_preference = ""
+                scale_unit         = 1
+              }
+            }
+            azure_firewall = {
+              enabled = false
+              config = {
+                enable_dns_proxy              = false
+                dns_servers                   = []
+                sku_tier                      = "Standard"
+                base_policy_id                = ""
+                private_ip_ranges             = []
+                threat_intelligence_mode      = ""
+                threat_intelligence_allowlist = []
+                availability_zones = {
+                  zone_1 = true
+                  zone_2 = true
+                  zone_3 = true
+                }
+              }
+            }
+            spoke_virtual_network_resource_ids        = []
+            secure_spoke_virtual_network_resource_ids = []
+            enable_virtual_hub_connections            = false
+          }
+        },
+      ]
+      ddos_protection_plan = {
+        enabled = false
+        config = {
+          location = ""
+        }
+      }
+      dns = {
+        enabled = true
+        config = {
+          location = ""
+          enable_private_link_by_service = {
+            azure_api_management                 = true
+            azure_app_configuration_stores       = true
+            azure_arc                            = true
+            azure_automation_dscandhybridworker  = true
+            azure_automation_webhook             = true
+            azure_backup                         = true
+            azure_batch_account                  = true
+            azure_bot_service_bot                = true
+            azure_bot_service_token              = true
+            azure_cache_for_redis                = true
+            azure_cache_for_redis_enterprise     = true
+            azure_container_registry             = true
+            azure_cosmos_db_cassandra            = true
+            azure_cosmos_db_gremlin              = true
+            azure_cosmos_db_mongodb              = true
+            azure_cosmos_db_sql                  = true
+            azure_cosmos_db_table                = true
+            azure_data_explorer                  = true
+            azure_data_factory                   = true
+            azure_data_factory_portal            = true
+            azure_data_health_data_services      = true
+            azure_data_lake_file_system_gen2     = true
+            azure_database_for_mariadb_server    = true
+            azure_database_for_mysql_server      = true
+            azure_database_for_postgresql_server = true
+            azure_digital_twins                  = true
+            azure_event_grid_domain              = true
+            azure_event_grid_topic               = true
+            azure_event_hubs_namespace           = true
+            azure_file_sync                      = true
+            azure_hdinsights                     = true
+            azure_iot_dps                        = true
+            azure_iot_hub                        = true
+            azure_key_vault                      = true
+            azure_key_vault_managed_hsm          = true
+            azure_kubernetes_service_management  = true
+            azure_machine_learning_workspace     = true
+            azure_managed_disks                  = true
+            azure_media_services                 = true
+            azure_migrate                        = true
+            azure_monitor                        = true
+            azure_purview_account                = true
+            azure_purview_studio                 = true
+            azure_relay_namespace                = true
+            azure_search_service                 = true
+            azure_service_bus_namespace          = true
+            azure_site_recovery                  = true
+            azure_sql_database_sqlserver         = true
+            azure_synapse_analytics_dev          = true
+            azure_synapse_analytics_sql          = true
+            azure_synapse_studio                 = true
+            azure_web_apps_sites                 = true
+            azure_web_apps_static_sites          = true
+            cognitive_services_account           = true
+            microsoft_power_bi                   = true
+            signalr                              = true
+            signalr_webpubsub                    = true
+            storage_account_blob                 = true
+            storage_account_file                 = true
+            storage_account_queue                = true
+            storage_account_table                = true
+            storage_account_web                  = true
+          }
+          private_link_locations                                 = []
+          public_dns_zones                                       = []
+          private_dns_zones                                      = []
+          enable_private_dns_zone_virtual_network_link_on_hubs   = true
+          enable_private_dns_zone_virtual_network_link_on_spokes = true
+          virtual_network_resource_ids_to_link                   = []
+        }
+      }
+    }
+    location = null
+    tags     = null
+    advanced = null
+  }
+
+
 }
